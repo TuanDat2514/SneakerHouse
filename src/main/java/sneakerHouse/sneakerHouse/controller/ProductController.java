1a62ffc5062776fc428ae8c8b7a27db4b9f6325c
thanglechiadamosoftcom <thang.lechi@adamosoft.com>
2022-11-14 14:17:22 +0700

    fiter prod by gender

--- src/main/java/sneakerHouse/sneakerHouse/controller/ProductController.java
+++ src/main/java/sneakerHouse/sneakerHouse/controller/ProductController.java
@@ -1,4 +1,4 @@
-/*
+    /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import sneakerHouse.sneakerHouse.entity.Brand;
 import sneakerHouse.sneakerHouse.entity.Product;
@@ -54,5 +55,10 @@ public class ProductController {
     @GetMapping("/getProdbyId/{id_product}")
     public Optional<Product> getList(@PathVariable String id_product){
         return productService.getProductbyId(id_product);
+    }
+    
+    @GetMapping("/getProduct")
+    public List<Product> getProdbyGender(@RequestParam int gender){
+        return productService.listProductbyGender(gender);
     }
 }
